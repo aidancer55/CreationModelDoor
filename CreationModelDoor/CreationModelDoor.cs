9ad57305e80abc166cff8d1cf6ddeb8a2e0937d3
aidancer55 <bluebonnets77@gmail.com>
2022-05-07 15:48:50 +0300

    1

--- CreationModelDoor/CreationModelDoor.cs
+++ CreationModelDoor/CreationModelDoor.cs
@@ -17,8 +17,17 @@ namespace CreationModePlugin
         {
             Document doc = commandData.Application.ActiveUIDocument.Document;
 
-            Level level1, level2;
-            Levels(doc, out level1, out level2);
+            List<Level> listLevel = new FilteredElementCollector(doc)
+                .OfClass(typeof(Level))
+                .OfType<Level>()
+                .ToList();
+
+            Level level1 = listLevel
+                .Where(e => e.Name.Equals("Уровень 1"))
+                .FirstOrDefault();
+            Level level2 = listLevel
+                .Where(e => e.Name.Equals("Уровень 2"))
+                .FirstOrDefault();
 
             double width = UnitUtils.ConvertToInternalUnits(10000, UnitTypeId.Millimeters);
             double depth = UnitUtils.ConvertToInternalUnits(5000, UnitTypeId.Millimeters);
@@ -33,16 +42,43 @@ namespace CreationModePlugin
             points.Add(new XYZ(-dx, -dy, 0));
 
             List<Wall> walls = new List<Wall>();
+            List<FamilySymbol> windows = new List<FamilySymbol>();
 
             Transaction transaction = new Transaction(doc, "Построение стен");
             transaction.Start();
 
             Walls(doc, level1, level2, points, walls);
             AddDoor(doc, level1, walls[0]);
+            AddWindow(doc, level1, walls[1]);
+            AddWindow(doc, level1, walls[2]);
+            AddWindow(doc, level1, walls[3]);
 
             transaction.Commit();
 
             return Result.Succeeded;
+        }
+
+        private void AddWindow(Document doc, Level level1, Wall wall)
+        {
+            FamilySymbol windowType = new FilteredElementCollector(doc)
+                .OfClass(typeof(FamilySymbol))
+                .OfCategory(BuiltInCategory.OST_Windows)
+                .OfType<FamilySymbol>()
+                .Where(x => x.Name.Equals("0915 x 1220 мм"))
+                .Where(x => x.FamilyName.Equals("Фиксированные"))
+                .FirstOrDefault();
+
+            LocationCurve hostCurve = wall.Location as LocationCurve;
+            XYZ point1 = hostCurve.Curve.GetEndPoint(0);
+            XYZ point2 = hostCurve.Curve.GetEndPoint(1);
+            XYZ point = (point1 + point2) / 2;
+            
+            if (!windowType.IsActive)
+                windowType.Activate();
+
+            FamilyInstance window = doc.Create.NewFamilyInstance(point, windowType, wall, level1, StructuralType.NonStructural);
+            Parameter offset = window.get_Parameter(BuiltInParameter.INSTANCE_SILL_HEIGHT_PARAM);
+            offset.Set(UnitUtils.ConvertToInternalUnits(800, UnitTypeId.Millimeters));
         }
 
         private static void Walls(Document doc, Level level1, Level level2, List<XYZ> points, List<Wall> walls)
@@ -54,21 +90,6 @@ namespace CreationModePlugin
                 walls.Add(wall);
                 wall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id);
             }
-        }
-
-        private static void Levels(Document doc, out Level level1, out Level level2)
-        {
-            List<Level> listLevel = new FilteredElementCollector(doc)
-                .OfClass(typeof(Level))
-                .OfType<Level>()
-                .ToList();
-
-            level1 = listLevel
-                .Where(e => e.Name.Equals("Уровень 1"))
-                .FirstOrDefault();
-            level2 = listLevel
-                .Where(e => e.Name.Equals("Уровень 2"))
-                .FirstOrDefault();
         }
 
         private void AddDoor(Document doc, Level level1, Wall wall)
@@ -86,7 +107,7 @@ namespace CreationModePlugin
             XYZ point2 = hostCurve.Curve.GetEndPoint(1);
             XYZ point = (point1 + point2) / 2;
 
-            if(!doorType.IsActive)
+            if (!doorType.IsActive)
                 doorType.Activate();
 
             doc.Create.NewFamilyInstance(point, doorType, wall, level1, StructuralType.NonStructural);
